a6cb19f7cf1f4f8adab62be39073b4a5cbaee222
Felipe Alvarez <felipe.alvarez@itoc.com.au>
2017-05-10 09:10:05 +1000

    credstash: make it into a module, not a role
    Include a brief example which now includes 'autoversion' feature.
    Removed references to credstash "role" as it is now just a module in
    library dir and a playbook which can be included in the main playbook.

--- library/credstash.py
+++ library/credstash.py
@@ -5,29 +5,60 @@ import json
 import credstash
 import yaml
 from ansible.module_utils.basic import *
+class Arguments:
+  def __init__(self, Args_dict):
+    self.autoversion = Args_dict['autoversion']
+    self.context = Args_dict['context']
+    self.digest = Args_dict['digest']
+    self.fact = Args_dict['fact']
+    self.fact_type = Args_dict['fact_type']
+    self.credential = Args_dict['credential']
+    self.key = Args_dict['key']
+    self.mode = Args_dict['mode']
+    self.region = Args_dict['region']
+    self.table = Args_dict['table']
+    self.value = Args_dict['value']
+    self.version = Args_dict['version']
 
 def main():
   module = AnsibleModule(
     argument_spec = dict(
-      secret = dict(required=True, type='str'),
+      autoversion = dict(default=False, type='bool'),
+      context = dict(default=None, type='dict'),
+      digest = dict(default='SHA256', type='str'),
       fact = dict(default=None, type='str'),
       fact_type = dict(default=None, choices=['yaml','json']),
-      mode = dict(default='get', choices=['get', 'put']),
-      value = dict(default=None, type='str'),
       key = dict(default='alias/credstash', type='str'),
+      mode = dict(default='get', choices=['get', 'put']),
       region = dict(default='us-east-1', type='str'),
+      secret = dict(required=True, type='str'),
       table = dict(default='credential-store', type='str'),
+      value = dict(default=None, type='str'),
       version = dict(default='', type='str'),
-      context = dict(default=None, type='dict')
     )
   )
+
+  args_dict = {
+    'autoversion'   : module.params.get('autoversion'),
+    'context'       : module.params.get('context'),
+    'digest'        : module.params.get('digest'),
+    'fact'          : module.params.get('fact'),
+    'fact_type'     : module.params.get('fact_type'),
+    'credential'    : module.params.get('secret'),
+    'key'           : module.params.get('key'),
+    'mode'          : module.params.get('mode'),
+    'region'        : module.params.get('region'),
+    'table'         : module.params.get('table'),
+    'value'         : module.params.get('value'),
+    'version'       : module.params.get('version'),
+  }
+
+  args = Arguments(args_dict)
 
   result = dict(changed=False, failed=False)
 
   if module.params.get('mode') == 'put':
-    result['output'] = credstash.putSecret(module.params.get('secret'), module.params.get('value'), \
-      module.params.get('version'), module.params.get('key'), module.params.get('region'), \
-      module.params.get('table'), module.params.get('context'))
+    result['output'] = credstash.putSecretAction(args, args.region)
     result['changed'] = True
 
   try: 
@@ -46,6 +77,4 @@ def main():
 
   module.exit_json(**result)
 
-# import module snippets
-from ansible.module_utils.basic import *
 main()
